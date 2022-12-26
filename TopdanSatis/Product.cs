802d9c9cac255baf529cb47d60ec0308e1df6bc9
MehemmedIsmayilov <mehemmedmi@code.edu.az>
2022-12-26 17:12:11 +0400

    Tapsiriq

---
+++ TopdanSatis/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopdanSatis
+{
+    internal class Product
+    {
+
+        public int No;
+        public string Name;
+        public double Price;
+        public Products Type;
+
+        public Product(int no,string name,double price,Products type)
+        {
+            No = no;
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+
+
+
+   
+    }
+}
---
+++ TopdanSatis/Store.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TopdanSatis
+{
+    internal class Store 
+    {
+        Product[] arrProducts;
+
+
+        public Store()
+        {
+            arrProducts = new Product[0];
+        }
+        public void AddProduct()
+        {
+            string namem = Console.ReadLine();
+            double pricem = Convert.ToDouble(Console.ReadLine());
+            Products type;
+            //
+            geridon:
+            string typestr= Console.ReadLine().ToLower();
+            
+            switch (typestr)
+            {
+                case "meat":
+                    type = Products.Meat;
+                    break;
+                case "drink":
+                    type = Products.Drink;
+                        break;
+                case "baker":
+                    type = Products.Baker;
+                    break;
+                default:
+                    Console.WriteLine("Uygun simvol daxil edin");
+                    goto geridon;
+                    //
+                    break;
+            }
+
+            Product product = new Product(namem, pricem, type);
+            Array.Resize(ref arrProducts, arrProducts.Length + 1);
+            arrProducts[arrProducts.Length - 1] = product;
+            Console.WriteLine("Elave Olundu");
+            
+
+        }
+
+        public void FilterProductByName()
+        {
+
+            for (int i = 0; i < arrProducts.Length; i++)
+
+            {
+                
+
+            }
+        }
+
+
+       
+
+        public void FilterProductByType(Products type, string[] arrProducts)
+        {
+            int count = 0;
+            Products products;
+            string typestr = Console.ReadLine().ToLower();
+            switch (typestr)
+            {
+                case "meat":
+                    type = Products.Meat;
+                    break;
+                case "drink":
+                    type = Products.Drink;
+                    break;
+                case "baker":
+                    type = Products.Baker;
+                    break;
+
+
+
+                default:
+                    break;
+            }
+            foreach (var item in arrProducts)
+            {
+                if (item.Type == type )             //hell ede bilmedim
+                {
+                    count++;
+                    Console.WriteLine(count + " " + item);
+                }
+
+            }
+
+          
+
+        }
+
+        public void RemoveProductByNo(int no, string[]arrProducts)
+        {
+            for (int i = no-1; i < arrProducts.Length; i++)
+            {
+
+            }
+        }
+    }
+}
